f7cce20fa76df4dcf1dd01ed9aa35d3bc37fb76c
Ingridbizlution <ingrid@bizlution.com>
2026-03-25 21:05:41 +0800

    push new vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/signcms_new_node/',
+  // base: '/signcms_new_node/',
+  base: mode === 'production' ? '/signcms_new_node/' : '/',
   server: {
     host: "::",
     port: 8080,
